8fa2df9380d4f5b65a2e71004b7265bc255f53ed
Canaga81 <Canagamemmedov@gmail.com>
2024-03-12 09:45:13 +0400

    Add Data Error & Loading

--- src/components/Rated/Rated.jsx
+++ src/components/Rated/Rated.jsx
@@ -3,7 +3,7 @@ import { DataContext } from "../../contexts/DataContext";
 import Card from "../Uİ/Card";
 
 const Rated = () => {
-  const { products, fetchProductsHandler } = useContext(DataContext);
+  const { products, fetchProductsHandler, isLoading, error } = useContext(DataContext);
   //   console.log(fetchProductsHandler);
   //   console.log(products);
 
@@ -26,9 +26,14 @@ const Rated = () => {
             </h1>
           </div>
           <div className="flex flex-wrap justify-center gap-9">
-            {products.map(({ title, image, id, price }) => (
+            {isLoading && <p className="text-center font-Montserrat text-heroText tracking-[1.5px] lg:text-[45px] md:text-[39px] sm:text-[30px] text-[25px]">Loading...</p>}
+            {!isLoading && error && <p className="text-center font-Montserrat text-red-600 tracking-[1.5px] lg:text-[45px] md:text-[39px] sm:text-[30px] text-[25px]">{error}</p>}
+            {!isLoading && products.length > 0 && products.map(({ title, image, id, price }) => (
               <Card key={id} title={title} image={image} price={price} />
             ))}
+            {
+              !isLoading && !error && products.length === 0 && <p className="text-center font-Montserrat text-heroText tracking-[1.5px] lg:text-[45px] md:text-[39px] sm:text-[30px] text-[25px]">Hec bir Mehsul Yoxdur !</p>
+            }
           </div>
           <div>
             <button className="lg:text-[24px] md:text-[21px] sm:text-[19px] text-[16px] border-none outline-none bg-heroText py-2 px-10 rounded-lg transition duration-200 opacity-80 hover:opacity-100 text-white font-Montserrat">
--- src/contexts/DataContext.jsx
+++ src/contexts/DataContext.jsx
@@ -1,18 +1,31 @@
-import { useState, createContext, useEffect, useCallback } from "react";
+import { useState, createContext, useCallback } from "react";
 
 export const DataContext = createContext();
 
 const DataProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const fetchProductsHandler = useCallback(() => {
-    fetch("http://localhost:3000/flowers")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setProducts(data);
-      });
+  const fetchProductsHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("http://localhost:3000/flowers");
+
+      if (response.status !== 200) {
+        throw new Error("Data'da yanlisliq var !");
+      }
+
+      const data = await response.json();
+
+      setProducts(data);
+    } catch (error) {
+      setError(error.message)
+    }
+
+    setIsLoading(false);
   }, []);
 
   // useEffect(() => {
@@ -20,7 +33,7 @@ const DataProvider = ({ children }) => {
   // }, [fetchProductsHandler]);
 
   return (
-    <DataContext.Provider value={{ fetchProductsHandler, products }}>
+    <DataContext.Provider value={{ fetchProductsHandler, products, isLoading, error }}>
       {children}
     </DataContext.Provider>
   );
