54fca2c4a9a552498c448eb0db65b783c2aae46c
Canaga81 <Canagamemmedov@gmail.com>
2024-03-17 10:45:31 +0400

    App ''

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Footer from "./components/Footer/Footer";
 import { motion } from 'framer-motion';
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
+
 function App() {
 
   return (
@@ -53,7 +54,7 @@ function App() {
     </motion.div>
 
   );
-  
+
 
 }
 
